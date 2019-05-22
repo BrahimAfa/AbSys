a69eecd55409faff39aad328cf21699e6091f5d3
Brahim Afassy <brahimafassy@gmail.com>
2019-05-22 00:26:01 +0000

    Initializing Project Files

---
+++ app/src/main/java/com/ofppt/absys/UI/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.ofppt.absys.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ofppt.absys.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
