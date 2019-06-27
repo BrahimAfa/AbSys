7e9a14b4b86de29f646c4f954917ce5839a7ba52
Brahim Afassy <brahimafassy@gmail.com>
2019-06-27 18:09:45 +0100

    Creating Absence Activity and adding Helper dependencies

---
+++ app/src/main/java/com/ofppt/absys/Main/UI/Absence.java
@@ -0,0 +1,16 @@
+package com.ofppt.absys.Main.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ofppt.absys.R;
+
+public class Absence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absence);
+    }
+}
