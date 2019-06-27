29ef11ae22b66e1ef8ec9738979ca047a338c590
Brahim Afassy <brahimafassy@gmail.com>
2019-06-27 19:26:38 +0100

    Adding Constant Class with Static vars and Interface Package with IOnChecked

---
+++ app/src/main/java/com/ofppt/absys/Main/Constants/Constants.java
@@ -0,0 +1,21 @@
+package com.ofppt.absys.Main.Constants;
+
+import com.ofppt.absys.Main.Models.STAGIAIRES;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+    public static final List<STAGIAIRES> CheckedStudentSC1 = new ArrayList<>();
+    public static final List<STAGIAIRES> CheckedStudentSC2 = new ArrayList<>();
+    public static final int TYPE_HEAD = 0;
+    public static final int TYPE_LIST = 1;
+    public static   int AM_PM = -1;
+    public static  final String Seance1 = "S1";
+    public static  final String Seance2 = "S2";
+    public static  final String Seance3 = "S3";
+    public static  final String Seance4 = "S4";
+
+
+
+}
---
+++ app/src/main/java/com/ofppt/absys/Main/Interfaces/IOnChecked.java
@@ -0,0 +1,6 @@
+package com.ofppt.absys.Main.Interfaces;
+
+public interface IOnChecked {
+    void onClick(int position);
+    void OnChecked(int position);
+}
