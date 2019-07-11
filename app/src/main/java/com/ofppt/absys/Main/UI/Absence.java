921fc81e634a84b65c9dadc21f94e04e8264413d
Brahim Afassy <brahimafassy@gmail.com>
2019-07-11 19:49:57 +0100

    Inserting To GroupENg to See If a group Is Been Verified By a SuperVisor

--- app/src/main/java/com/ofppt/absys/Main/UI/Absence.java
+++ app/src/main/java/com/ofppt/absys/Main/UI/Absence.java
@@ -13,6 +13,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +30,7 @@ import com.ofppt.absys.Main.Interfaces.IOnInputListenner;
 import com.ofppt.absys.Main.Models.ABSENCES;
 import com.ofppt.absys.Main.Models.FORMATEURS;
 import com.ofppt.absys.Main.Models.GROUPES;
+import com.ofppt.absys.Main.Models.GroupEnrg;
 import com.ofppt.absys.Main.Models.STAGIAIRES;
 import com.ofppt.absys.R;
 
@@ -40,7 +44,8 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
     TextView fili;
     private RecyclerView Rview;
     private FloatingActionButton fabValidate;
-
+    private AutoCompleteTextView FormateurComplete;
+    String ExtraCodeGoup;
     //Vars
     List<STAGIAIRES> listG;
     @Override
@@ -80,6 +85,8 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
         }
         group = findViewById(R.id.txtGroup);
         fili = findViewById(R.id.txtFilier);
+        FormateurComplete = findViewById(R.id.FormateurComplete);
+
         ActiveAndroid.initialize(this);
         Calendar cal = Calendar.getInstance();
         Constants.AM_PM = cal.get(Calendar.AM_PM);
@@ -89,9 +96,9 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-        String Code = extras.getString("codeg");
+        ExtraCodeGoup = extras.getString("codeg");
         String fil = extras.getString("Filiere");
-        group.setText(Code);
+        group.setText(ExtraCodeGoup);
         fili.setText(fil);
         fabValidate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,8 +107,17 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
             }
         });
 //        RecyclerAdapter adapter = new RecyclerAdapter(STAGIAIRES.getAll(),this,this);
-        listG = STAGIAIRES.getbyGroup(GROUPES.getbycodeGroup(Code));
+        listG = STAGIAIRES.getbyGroup(GROUPES.getbycodeGroup(ExtraCodeGoup));
         InitializingRecyclerView();
+        FormateurComplete.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, FORMATEURS.getAllNmaes()));
+
+        FormateurComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Constants.FOMATEUR = FORMATEURS.getAll().get(position);
+                Toast.makeText(getApplicationContext(),Constants.FOMATEUR._Nom+" " +Constants.FOMATEUR._Prenom+"Test",Toast.LENGTH_LONG).show();
+            }
+        });
     }
     private void InitializingRecyclerView(){
         RecyclerAdapter adapter = new RecyclerAdapter(listG,this,this);
@@ -109,9 +125,34 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
         Rview.setAdapter(adapter);
         Rview.setLayoutManager(new LinearLayoutManager(this));
         Rview.addItemDecoration(divider);
+        Constants.CheckedStudentSC1.clear();
+        Constants.CheckedStudentSC2.clear();
+        Constants.AucunSc1State = false;
+        Constants.AucunSc2State = false;
+    }
+    private void InitializingRecyclerView(String Seance,boolean checkState){
+        RecyclerAdapter adapter = new RecyclerAdapter(listG,this,this,Seance,checkState);
+      //  DividerItemDecoration divider = new DividerItemDecoration(this,new LinearLayoutManager(this).getOrientation());
+        Rview.setAdapter(adapter);
+   //     Rview.setLayoutManager(new LinearLayoutManager(this));
+      //  Rview.addItemDecoration(divider);
+        Constants.CheckedStudentSC1.clear();
+        Constants.CheckedStudentSC2.clear();
     }
     public void ShowDialog() {
-        if (Constants.CheckedStudentSC1.size()!=0 | Constants.CheckedStudentSC2.size()!=0)
+        if (Constants.AucunSc1State & Constants.AucunSc2State) {
+            ConfirmationDialog confirmD = new ConfirmationDialog();
+            confirmD.show(getSupportFragmentManager(),"Confirmation Dialog");
+        }
+        else if (Constants.AucunSc1State & Constants.CheckedStudentSC2.size() > 0) {
+            ConfirmationDialog confirmD = new ConfirmationDialog();
+            confirmD.show(getSupportFragmentManager(),"Confirmation Dialog");
+        }
+        else if (Constants.AucunSc2State & Constants.CheckedStudentSC1.size() > 0) {
+            ConfirmationDialog confirmD = new ConfirmationDialog();
+            confirmD.show(getSupportFragmentManager(),"Confirmation Dialog");
+        }
+        else if (Constants.CheckedStudentSC1.size()!=0 | Constants.CheckedStudentSC2.size()!=0)
         {
             ConfirmationDialog confirmD = new ConfirmationDialog();
             confirmD.show(getSupportFragmentManager(),"Confirmation Dialog");
@@ -135,15 +176,28 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
         }
         ActiveAndroid.beginTransaction();
         try {
-            for (STAGIAIRES stgS: Constants.CheckedStudentSC1) {
-
-                new ABSENCES(stgS,formateur,Calendar.getInstance().getTime(),S1).save();
-               stgS.UpdateAbsenceComule();
+            if (Constants.AucunSc1State) {
+                new GroupEnrg(GROUPES.getbycodeGroup(ExtraCodeGoup),Calendar.getInstance().getTime(),S1,Constants.FOMATEUR).save();
+            }
+            if ( Constants.CheckedStudentSC1.size()>0) {
+                for (STAGIAIRES stgS: Constants.CheckedStudentSC1) {
+                    new ABSENCES(stgS,formateur,Calendar.getInstance().getTime(),S1).save();
+                    stgS.UpdateAbsenceComule();
+                }
+                new GroupEnrg(GROUPES.getbycodeGroup(ExtraCodeGoup),Calendar.getInstance().getTime(),S1,Constants.FOMATEUR).save();
 
             }
-            for (STAGIAIRES stgS: Constants.CheckedStudentSC2) {
-                new ABSENCES(stgS,formateur,Calendar.getInstance().getTime(),S2).save();
-                stgS.UpdateAbsenceComule();
+            if (Constants.AucunSc2State) {
+                new GroupEnrg(GROUPES.getbycodeGroup(ExtraCodeGoup),Calendar.getInstance().getTime(),S2,Constants.FOMATEUR).save();
+            }
+            if (Constants.CheckedStudentSC2.size() > 0) {
+
+                for (STAGIAIRES stgS : Constants.CheckedStudentSC2) {
+                    new ABSENCES(stgS, formateur, Calendar.getInstance().getTime(), S2).save();
+                    stgS.UpdateAbsenceComule();
+                    new GroupEnrg(GROUPES.getbycodeGroup(ExtraCodeGoup),Calendar.getInstance().getTime(),S2,Constants.FOMATEUR).save();
+
+                }
             }
             ActiveAndroid.setTransactionSuccessful();
         }
@@ -168,6 +222,23 @@ public class Absence extends AppCompatActivity implements IOnInputListenner , IO
 
     @Override
     public void OnChecked(int position) {
+
+    }
+
+    @Override
+    public void OnAucunCheckedSC1(Boolean CheckState) {
+        Toast.makeText(this,"Test Acune SC1",Toast.LENGTH_SHORT).show();
+        InitializingRecyclerView(Constants.Seance1,CheckState);
+        Constants.CheckedStudentSC1.clear();
+
+    }
+
+    @Override
+    public void OnAucunCheckedSC2(Boolean CheckState) {
+        Toast.makeText(this,"Test Acune SC2",Toast.LENGTH_SHORT).show();
+        InitializingRecyclerView(Constants.Seance2,CheckState);
+        Constants.CheckedStudentSC2.clear();
+
 
     }
 }
